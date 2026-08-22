23a0a6056aba1c01207b34246de8bf44eef53a46
Pi5 <achicir2@gmail.com>
2026-08-22 18:04:27 +0700

    feat: add dongle screen background

---
+++ boards/shields/charybdis/src/custom_status_screen.c
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2024 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "custom_status_screen.h"
+
+extern const lv_image_dsc_t background_img;
+
+#if CONFIG_DONGLE_SCREEN_OUTPUT_ACTIVE
+#include "widgets/output_status.h"
+static struct zmk_widget_output_status output_status_widget;
+#endif
+
+#if CONFIG_DONGLE_SCREEN_LAYER_ACTIVE
+#include "widgets/layer_status.h"
+static struct zmk_widget_layer_status layer_status_widget;
+#endif
+
+#if CONFIG_DONGLE_SCREEN_BATTERY_ACTIVE
+#include "widgets/battery_status.h"
+static struct zmk_widget_dongle_battery_status dongle_battery_status_widget;
+#endif
+
+#if CONFIG_DONGLE_SCREEN_WPM_ACTIVE
+#include "widgets/wpm_status.h"
+static struct zmk_widget_wpm_status wpm_status_widget;
+#endif
+
+#if CONFIG_DONGLE_SCREEN_MODIFIER_ACTIVE
+#include "widgets/mod_status.h"
+static struct zmk_widget_mod_status mod_widget;
+#endif
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+lv_style_t global_style;
+
+static void make_widget_transparent(lv_obj_t *obj) {
+    lv_obj_set_style_bg_opa(obj, LV_OPA_TRANSP, LV_PART_MAIN);
+    lv_obj_set_style_border_width(obj, 0, LV_PART_MAIN);
+}
+
+static void add_background(lv_obj_t *screen) {
+    lv_obj_t *background = lv_image_create(screen);
+    lv_image_set_src(background, &background_img);
+    lv_obj_align(background, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_move_to_index(background, 0);
+}
+
+lv_obj_t *zmk_display_status_screen() {
+    lv_obj_t *screen = lv_obj_create(NULL);
+    lv_obj_set_style_bg_opa(screen, LV_OPA_TRANSP, LV_PART_MAIN);
+    add_background(screen);
+
+    lv_style_init(&global_style);
+    lv_style_set_text_color(&global_style, lv_color_white());
+    lv_style_set_text_letter_space(&global_style, 1);
+    lv_style_set_text_line_space(&global_style, 1);
+    lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
+
+#if CONFIG_DONGLE_SCREEN_OUTPUT_ACTIVE
+    zmk_widget_output_status_init(&output_status_widget, screen);
+    make_widget_transparent(zmk_widget_output_status_obj(&output_status_widget));
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_MID, 0, 10);
+#endif
+
+#if CONFIG_DONGLE_SCREEN_BATTERY_ACTIVE
+    zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
+    make_widget_transparent(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget));
+    lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget),
+                 LV_ALIGN_BOTTOM_MID, 0, 0);
+#endif
+
+#if CONFIG_DONGLE_SCREEN_WPM_ACTIVE
+    zmk_widget_wpm_status_init(&wpm_status_widget, screen);
+    make_widget_transparent(zmk_widget_wpm_status_obj(&wpm_status_widget));
+    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_TOP_LEFT, 20, 20);
+#endif
+
+#if CONFIG_DONGLE_SCREEN_LAYER_ACTIVE
+    zmk_widget_layer_status_init(&layer_status_widget, screen);
+    make_widget_transparent(zmk_widget_layer_status_obj(&layer_status_widget));
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_CENTER, 0, 0);
+#endif
+
+#if CONFIG_DONGLE_SCREEN_MODIFIER_ACTIVE
+    zmk_widget_mod_status_init(&mod_widget, screen);
+    make_widget_transparent(zmk_widget_mod_status_obj(&mod_widget));
+    lv_obj_align(zmk_widget_mod_status_obj(&mod_widget), LV_ALIGN_CENTER, 0, 35);
+#endif
+
+    return screen;
+}
